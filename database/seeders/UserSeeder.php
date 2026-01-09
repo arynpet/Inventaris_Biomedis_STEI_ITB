ea0f7ccf25197e7cc56377e6fa5d52947b5aa674
arynpet <muhammad.aryan.v.p@gmail.com>
2026-01-09 09:13:09 +0700

    chore(database): add superadmin user to seeder
    Add a default superadmin account to UserSeeder to facilitate access control testing and initial system setup.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,17 +11,18 @@ class UserSeeder extends Seeder
     {
         User::insert([
             [
-                'name' => 'Raden Satya Febry Pangestu',
-                'email' => 'radenstya2@gmail.com',
-                'email_verified_at' => null,
-                'password' => bcrypt('password'),
-                'created_at' => '2025-12-04 17:41:40',
-                'updated_at' => '2025-12-04 17:41:40',
-            ],
-            [
                 'name' => 'admin',
                 'email' => 'admin@example.com',
                 'email_verified_at' => '2025-12-17 01:34:11',
+                'password' => bcrypt('password'),
+                'created_at' => '2025-12-17 01:34:11',
+                'updated_at' => '2025-12-17 01:34:11',
+            ],
+            [
+                'name' => 'superadmin',
+                'email' => 'superadmin@example.com',
+                'email_verified_at' => '2025-12-17 01:34:11',
+                'role' => 'superadmin',
                 'password' => bcrypt('password'),
                 'created_at' => '2025-12-17 01:34:11',
                 'updated_at' => '2025-12-17 01:34:11',
